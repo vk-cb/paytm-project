9b1c33940ecf1251568aee4d450836b30250b699
Vicky Gupta <vicky.k.g@thecodebucket.com>
2024-08-03 15:52:03 +0530

    get-profile and userdetails by id api created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { userSignup, userSignin, addInWallet, transferMoney } = require('../controllers/userConroller');
+const { userSignup, userSignin, addInWallet, transferMoney, getUserById, getProfile } = require('../controllers/userConroller');
 const { userAuthMiddleware } = require('../middleware/userMiddleware');
 
 const router = express.Router();
@@ -8,5 +8,7 @@ router.post('/signup', userSignup)
 router.post('/signin', userSignin)
 router.post('/add',userAuthMiddleware, addInWallet)
 router.post('/transfer',userAuthMiddleware, transferMoney)
+router.get('/user-details/:id',userAuthMiddleware, getUserById)
+router.get('/get-profile',userAuthMiddleware, getProfile)
 
 module.exports = router;
